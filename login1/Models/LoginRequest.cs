db6664112de7f66e5398f7686402997964b199f9
Nandhish K <H4EJ@it.eurofinseu.com>
2026-04-13 10:39:58 +0530

    Added the Refresh Tokens and Acess tokens features

--- login1/Models/LoginRequest.cs
+++ login1/Models/LoginRequest.cs
@@ -1,9 +1,16 @@
-﻿namespace login1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace login1.Models
 {
     public class LoginRequest
     {
-        public string Username { get; set; }
-        public string Password { get; set; }
+        [Required]
+        [MinLength(3)]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; } = string.Empty;
 
         public int Role_id { get; set; } // 1 for admin, 2 for user, 3 for viewer
     }
