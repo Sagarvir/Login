854ea6c5378ce224d09a8aef35aacf6381d12209
Nandhish K <H4EJ@it.eurofinseu.com>
2026-04-13 11:10:31 +0530

    Solved the Non-nullable property bug in the LoginRequest Model

--- login1/Models/LoginRequest.cs
+++ login1/Models/LoginRequest.cs
@@ -1,16 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
-
 namespace login1.Models
 {
     public class LoginRequest
     {
-        [Required]
-        [MinLength(3)]
-        public string Username { get; set; } = string.Empty;
-
-        [Required]
-        [MinLength(6)]
-        public string Password { get; set; } = string.Empty;
+        public string? Username { get; set; }
+        public string? Password { get; set; }
 
         public int Role_id { get; set; } // 1 for admin, 2 for user, 3 for viewer
     }
